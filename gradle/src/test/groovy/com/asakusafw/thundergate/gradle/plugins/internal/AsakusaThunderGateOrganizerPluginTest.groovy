ed745e2b10c28907dda7e9d19a41d7057c0c54cc
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2019-07-25 16:33:32 +0900

    Change default of ThunderGate Organizer enabled to true

--- gradle/src/test/groovy/com/asakusafw/thundergate/gradle/plugins/internal/AsakusaThunderGateOrganizerPluginTest.groovy
+++ gradle/src/test/groovy/com/asakusafw/thundergate/gradle/plugins/internal/AsakusaThunderGateOrganizerPluginTest.groovy
@@ -66,7 +66,7 @@ class AsakusaThunderGateOrganizerPluginTest extends OrganizerTestRoot {
     @Test
     public void extension_defaults() {
         AsakusafwOrganizerThunderGateExtension extension = project.asakusafwOrganizer.thundergate
-        assert extension.enabled == false
+        assert extension.enabled == true
         assert extension.target == null
     }
 
